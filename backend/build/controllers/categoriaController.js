f4253ad8ee9d83bb2017c3cf55f1cc1dd17cec14
dolivare5 <deiverolivares2@gmail.com>
2022-08-30 08:40:43 -0500

    Revert "Primer Commit. En este se comenzó a trabajar en el backend con typeScript, Node, Sequelize y Postgres."
    This reverts commit 59cf11f541b169e250c7e56ca4a96c93dec789b8.

--- backend/build/controllers/categoriaController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getCategories = (_req, res) => {
-    res.json({ msg: "GetCategories " });
-};
-exports.default = getCategories;
-//# sourceMappingURL=categoriaController.js.map
